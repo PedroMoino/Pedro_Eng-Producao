1d998125f2ea33b641aa1be583d4b677b473410e
PedroMoino <pedroaugustomoino@gmail.com>
2024-10-17 20:14:15 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"pedroaugustomoino@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
