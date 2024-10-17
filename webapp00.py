fa7b73b8efe1519edcb3da83c76d48fc4a60f477
PedroMoino <pedroaugustomoino@gmail.com>
2024-10-17 20:41:15 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -11,13 +11,13 @@ Configurar_Pagina("Exemplo 1",
                     "©️")
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
+st.title("BCF PLÁSTICOS LTDA")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Pedro Moino")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("PORTAS SANFONADAS")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
