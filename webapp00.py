1b6fd66d9dc3df0c2d45435698b10b5d78fbfeac
PedroMoino <pedroaugustomoino@gmail.com>
2024-11-13 20:27:07 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,7 +1,7 @@
 # MEU PRIMEIRO WEB APP
 import streamlit as st
 from ACTlib01 import *
-url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0U488MF4q5_1rWyEXTZ1OE3t_ACIylHTirxkI3-7usaYpu4KGjpnh4e7kIEjtahEJLKg8xrSQNhSx/pub?gid=786087389&single=true&output=csv"
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0U488MF4q5_1rWyEXTZ1OE3t_ACIylHTirxkI3-7usaYpu4KGjpnh4e7kIEjtahEJLKg8xrSQNhSx/pub?output=csv"
 db = Ler_GooglePlanilha(url)    
 Escrever(db)
 Configurar_Pagina("Exemplo 1", 
